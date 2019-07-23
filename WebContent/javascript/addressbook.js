2c698eaecc0cfea0464ab8f472d25db3b640f79e
Muhammad Wasi <muhammadwasi228@ymail.com>
2019-07-23 14:57:19 +0500

    Adding some initial files

---
+++ WebContent/javascript/addressbook.js
@@ -0,0 +1,34 @@
+var AddressBook = function(){
+    var init =function(){
+        ko.applyBindings(AddressBook);
+    }
+    //call the init once jquery loaded
+    $(init);
+
+    //array to holds contact objects
+    var contacts =ko.observableArray();
+
+    //declaring contact object
+    var contact ={
+        name: ko.observable(),
+        phoneNumber : ko.observable()
+    }
+
+    //Adding contact object(name, phoneNumber) to Contacts array
+    var addContact =function(){
+        contacts.push({name: contact.name(),phoneNumber: contact.phoneNumber()})
+        console.log("Adding contact with "+contact.name() +" and "+contact.phoneNumber());
+        clearContact()
+    }
+
+    //Clearing the input fields of UI
+    var clearContact=function(){
+        contact.name(null);
+        contact.phoneNumber(null)
+    }
+    return {
+        contact: contact,
+        addContact: addContact,
+        contacts: contacts
+    };
+}();
